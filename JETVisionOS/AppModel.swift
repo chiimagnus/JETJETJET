b6e1af9bf154129ec72b865bb9d9ef4892b88781
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <chii_magnus@outlook.com>
2025-09-09 23:24:00 +0800

    refactor(JET-AVP): 删除不再使用的视图和资源文件
    - 移除 ContentView.swift 和 ImmersiveView.swift 文件，清理不再需要的视图代码
    - 删除 Info.plist、JET_AVPApp.swift 及相关资源文件，简化项目结构
    - 该变更有助于提高代码可维护性和项目整洁性

---
+++ JETVisionOS/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  JETVisionOS
+//
+//  Created by chii_magnus on 2025/9/9.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ JETVisionOS/JETVisionOSApp.swift
@@ -0,0 +1,33 @@
+//
+//  JETVisionOSApp.swift
+//  JETVisionOS
+//
+//  Created by chii_magnus on 2025/9/9.
+//
+
+import SwiftUI
+
+@main
+struct JETVisionOSApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
