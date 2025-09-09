9ab57a06b6789f378fd80cf895be7c2049047014
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <chii_magnus@outlook.com>
2025-09-10 00:04:00 +0800

    refactor(JETVisionOS): 简化 ShowVolumetricJetButton 组件的按钮实现

--- JETVisionOS/Views/Volumetric/ShowVolumetricJetButton.swift
+++ JETVisionOS/Views/Volumetric/ShowVolumetricJetButton.swift
@@ -11,18 +11,9 @@ struct ShowVolumetricJetButton: View {
     @Environment(\.openWindow) private var openWindow
     
     var body: some View {
-        Button(action: {
-            // Open the volumetric window group
+        Button("Show Volumetric Jet") {
             openWindow(id: "volumetricJet")
-        }) {
-            Text("Show Volumetric Jet")
-                .font(.headline)
-                .padding()
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .cornerRadius(12)
         }
-        .padding(.top, 20)
     }
 }
 
