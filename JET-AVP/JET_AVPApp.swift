b29135b2e2a6afedfaae63e742ea45d5ed0db4be
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <chii_magnus@outlook.com>
2025-09-09 23:17:21 +0800

    feat(JET-AVP): 添加 MainView2D 作为新窗口视图
    - 在 JET_AVPApp 中新增 MainView2D 作为窗口组的一部分
    - 调整默认窗口大小设置，使用相对尺寸
    - 保留原有 ContentView 作为另一个窗口视图

--- JET-AVP/JET_AVPApp.swift
+++ JET-AVP/JET_AVPApp.swift
@@ -5,11 +5,15 @@ struct JET_AVPApp: App {
 
     var body: some Scene {
         WindowGroup {
+            MainView2D()
+        }
+
+        WindowGroup {
             ContentView()
         }
         .windowStyle(.volumetric)
-        // .defaultSize(width: 4096, height: 4096, depth: 4096)
-        
+        .defaultSize(width: 0.8, height: 0.8, depth: 0.8, in: .meters)
+
         ImmersiveSpace(id: "ImmersiveSpace"){
             ImmersiveView()
         }
